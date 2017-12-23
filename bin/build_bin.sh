e37d208ca4aac2c38b786eed883462a7a098194d
Mitch MacKenzie <mitchmac@gmail.com>
2017-12-23 11:11:43 -0400

    Get libcrypto because Lambda's version is old

--- bin/build_bin.sh
+++ bin/build_bin.sh
@@ -3,4 +3,5 @@
 docker build -t lambda-php7 .
 container=$(docker create lambda-php7)
 docker -D cp $container:/work/php-7-bin/bin/php-cgi ./php-cgi
+docker -D cp $container:/usr/lib64/libcrypto.so.1.0.2k ./lib/libcrypto.so.10
 docker rm $container
