d00d56a5762d6245e283b9c10196541758904966
Mitch MacKenzie <mitchmac@gmail.com>
2023-08-14 12:14:19 -0300

    Use https url for sqlite repo

--- test/build-test.sh
+++ test/build-test.sh
@@ -3,7 +3,7 @@
 cd ..
 mkdir temp
 cd temp
-git clone git@github.com:WordPress/sqlite-database-integration.git
+git clone https://github.com/WordPress/sqlite-database-integration.git
 cd ..
 docker build -t serverlesswp-test -f test/Dockerfile .
 rm -rf temp
