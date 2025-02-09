2c5864745e93e67f8ca9c13c5bcd823c603c98c7
Mitch MacKenzie <mitchmac@gmail.com>
2025-02-08 21:49:44 -0400

    Pass in sqlite env var

--- test/run.sh
+++ test/run.sh
@@ -2,7 +2,7 @@
 
 ./build-test.sh
 
-docker run -p 9000:8080 -d --name serverlesswp-test serverlesswp-test
+docker run -e FQDBDIR=/tmp/db -p 9000:8080 -d --name serverlesswp-test serverlesswp-test
 
 curl -s -o /dev/null -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"path":"/installer.php"}'
 
