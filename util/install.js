1fc7c819f697c28ff17fe511e929c057a2780041
Mitch MacKenzie <mitchmac@gmail.com>
2025-05-30 23:44:34 -0300

    Improve helper page (#96)

--- util/install.js
+++ util/install.js
@@ -1,6 +1,8 @@
 exports.validate = function(response) {
   let hasSqliteS3 = false;
   let hasSQL = false;
+  let platform = 'AWS';
+  let dashboardLink;
 
   if (process.env['SQLITE_S3_BUCKET']) {
     hasSqliteS3 = true;
@@ -11,9 +13,11 @@ exports.validate = function(response) {
   }
 
   if (process.env['SITE_NAME']) {
+    platform = 'Netlify';
     dashboardLink = `https://app.netlify.com/sites/${process.env['SITE_NAME']}/settings/env`;
   }
   else if (process.env['VERCEL']) {
+    platform = 'Vercel';
     dashboardLink = 'https://vercel.com/dashboard';
   }
   else {
@@ -21,38 +25,65 @@ exports.validate = function(response) {
   }
 
   if (!hasSQL && !hasSqliteS3) {
-    let message = "<p>It appears that the required environment variables for the WordPress database aren't setup.</p>"
-      + "<p>If you don't have a database solution selected yet, there are a few options.</p>"
-      + "<p>Head over to the <a href='https://github.com/mitchmac/ServerlessWP?tab=readme-ov-file#2-select-a-database-solution' target='_blank'>readme</a> to pick a data option and setup environment variables.</p>"
-      + `<p>You'll need to populate the environment variables for your site in the Vercel or Netlify (<a href="${dashboardLink}" target="_blank">dashboard</a>).</p>`
-      + '<p>Then <strong>remember to redeploy your site at Vercel or Netlify</strong> for the environment variables to be updated for the site.</p>';
+    let data = {};
+    data.dashboardLink = dashboardLink;
+    data.platform = platform;
 
+    data.database = process.env['DATABASE'] ? '✔️' : '❌';
+    data.username = process.env['USERNAME'] ? '✔️' : '❌';
+    data.password = process.env['PASSWORD'] ? '✔️' : '❌';
+    data.host = process.env['HOST'] ? '✔️' : '❌';
+ 
     return {
-      statusCode: 500,
+      statusCode: 200,
       headers: {
         'content-type': 'text/html; charset=utf-8'
       },
-      body: loadTemplate(message)
+      body: loadTemplate(data)
     }
   }
 }
 
-function loadTemplate(message){
-    return `
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-        <title>ServerlessWP WordPress Starter</title>
-      </head>
-      <body>
-        <main class="container" style="width: 800px; margin: 0 auto">
-          <h1>You're almost there!</h1>
-          ${message}
-        </main>
-      </body>
-    </html>
-    `;
+function loadTemplate(data) {
+return `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <title>ServerlessWP</title>
+    <style>
+      ul li {
+        list-style: none;
+      }
+    </style>
+  </head>
+  <body>
+    <main class="container" style="width: 800px; margin: 50px auto 0">
+      <p align="center" style="margin-bottom: 50px"><img src="https://serverlesswp.com/wp-content/serverlesswp.png"></p>
+      <p>ServerlessWP is installed!</p>
+      <p>Now we need to add a database for WordPress.</p>
+      <p>One of the easiest ways to do that right now is with a free <a href="https://www.pingcap.com/tidb-cloud-serverless/" target="_blank">TiDB cloud database</a>.</p>
+      <p>The following environment variables need to be populated in your project at ${data.platform} after creating a database.</p>
+      <ul>
+        <li>${data.database} DATABASE (the database's name)</li>
+        <li>${data.username} USERNAME (the username to connect to the database)</li>
+        <li>${data.password} PASSWORD (the password to connect to the database)</li>
+        <li>${data.host} HOST (the hostname to connect to the database)</li>
+      </ul>
+      <p>Go to your project's <a href="${data.dashboardLink}" target="_blank">dashboard</a> at ${data.platform} to make sure these values for your database details are entered. <strong>Then remember to re-deploy your project!</strong></p>
+      <br>
+      <br>
+      <details>
+        <summary>SQLite + S3 database alternative</summary>
+        <p>Want to try WordPress without a database server?</p>
+        <p>A super low cost and low maintenance way to handle WordPress data is to combine SQLite as a database with S3 storage. ServerlessWP does all of the hard work to keep the data in-sync.<p>
+        <p>Check out the project <a href="https://github.com/mitchmac/serverlesswp/?tab=readme-ov-file#sqlite--s3-database-option" target="_blank">readme</a> for more details.</p>
+      </details>
+      <br>
+      <br>
+      <sub>This page is auto-generated as part of the <a href="https://serverlesswp.com">ServerlessWP</a> installation.</sub>
+      </main>
+  </body>
+</html>`;
 }
